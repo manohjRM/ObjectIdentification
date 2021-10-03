b18adf9e059286ddfd3b78e906664293ed8e1037
manohjkumaar.rm <manohjkumaar.rm@gmail.com>
2021-10-03 19:54:26 +0530

    html update

--- ObjectIdentity/urls.py
+++ ObjectIdentity/urls.py
@@ -23,5 +23,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('upload/', include('imgUpload.urls'))
+    path('', include('imgUpload.urls'))
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
