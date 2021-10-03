0a07e0818ab9b3cf94b9e76faf7680bcfe115990
manohjkumaar.rm <manohjkumaar.rm@gmail.com>
2021-10-03 16:18:27 +0530

    update

--- ObjectIdentity/settings.py
+++ ObjectIdentity/settings.py
@@ -124,3 +124,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = "/static/"
--- ObjectIdentity/urls.py
+++ ObjectIdentity/urls.py
@@ -17,8 +17,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('upload/', include('imgUpload.urls'))
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
